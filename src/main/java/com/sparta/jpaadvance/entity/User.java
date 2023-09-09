d53e0e3e7571511d6e67e2c54d6021d19fcfe7f9
INYONG KIM <ohnyong87@gmail.com>
2023-09-09 23:40:40 +0900

    feat : @OneToOne two-way test -> check User entity

--- src/main/java/com/sparta/jpaadvance/entity/User.java
+++ src/main/java/com/sparta/jpaadvance/entity/User.java
@@ -13,4 +13,12 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
+
+    @OneToOne(mappedBy = "user")
+    private Food food;
+
+    public void addFood(Food food) { // 양방향 2 테스트 - 외래 키의 주인이 아닌 User 에서 Food 를 저장하기 위해 addFood() 메서드 추가
+        this.food = food;
+        food.setUser(this);
+    }
 }
