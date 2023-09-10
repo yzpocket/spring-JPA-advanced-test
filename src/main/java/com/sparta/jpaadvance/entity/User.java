cbdbd5443fa4273a93bbe7be7dbad39259e4a25b
INYONG KIM <ohnyong87@gmail.com>
2023-09-10 09:57:16 +0900

    feat : @ManyToOne two-way test

--- src/main/java/com/sparta/jpaadvance/entity/User.java
+++ src/main/java/com/sparta/jpaadvance/entity/User.java
@@ -4,6 +4,9 @@ import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Entity
 @Getter
 @Setter
@@ -21,4 +24,12 @@ public class User {
     //    this.food = food;
     //    food.setUser(this);
     //}
+
+    @OneToMany(mappedBy = "user")
+    private List<Food> foodList = new ArrayList<>();
+
+    public void addFoodList(Food food) {
+        this.foodList.add(food);
+        food.setUser(this); // 외래 키(연관 관계) 설정
+    }
 }
