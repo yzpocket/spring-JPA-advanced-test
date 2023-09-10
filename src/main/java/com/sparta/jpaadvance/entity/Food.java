b35e9a0c73834f145e51206037864414a8644fdc
INYONG KIM <ohnyong87@gmail.com>
2023-09-10 11:15:05 +0900

    feat : @ManyToMany one-way test

--- src/main/java/com/sparta/jpaadvance/entity/Food.java
+++ src/main/java/com/sparta/jpaadvance/entity/Food.java
@@ -26,7 +26,13 @@ public class Food {
     //@JoinColumn(name = "user_id")
     //private User user;
 
-    @OneToMany
-    @JoinColumn(name = "food_id") // users 테이블에 food_id 컬럼
+    //@OneToMany
+    //@JoinColumn(name = "food_id") // users 테이블에 food_id 컬럼
+    //private List<User> userList = new ArrayList<>();
+
+    @ManyToMany
+    @JoinTable(name = "orders", // 중간 테이블 생성
+            joinColumns = @JoinColumn(name = "food_id"), // 현재 위치인 Food Entity 에서 중간 테이블로 조인할 컬럼 설정
+            inverseJoinColumns = @JoinColumn(name = "user_id")) // 반대 위치인 User Entity 에서 중간 테이블로 조인할 컬럼 설정
     private List<User> userList = new ArrayList<>();
 }
